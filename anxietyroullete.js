03fb05060a627726fe6065e366a84182a2bf2be9
Leon <leon.potocki@gmail.com>
2025-06-02 11:15:12 +0200

    Update anxietyroullete.js

--- anxietyroullete.js
+++ anxietyroullete.js
@@ -40,6 +40,9 @@ function runRoullete() {
     setTimeout(() => {
         counter.innerText = "Streak " + localStorage.getItem('streak') || 0;
         progressBar.value = localStorage.getItem('streak') || 0;
+        if (localStorage.getItem('streak') == 10 {
+            startStrokeMode();
+        }
     }, 20000 / currentAudio.playbackRate); // Stop after 10 seconds
 }
 
